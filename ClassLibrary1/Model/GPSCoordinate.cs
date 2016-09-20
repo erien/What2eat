e8f83f997103c7fc28b9fe9280fcef69f5fabe45
Magdalena Glanowska <magdalena_glanowska@epam.com>
2016-09-20 16:43:14 +0200

    Main app template

---
+++ ClassLibrary1/Model/GPSCoordinate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eat.Core.Model
+{
+    // Let it be a struct for a performance reasons - object of struct is a value?
+    // It is an error to define a default (parameterless) constructor for a struct. 
+    // It is also an error to initialize an instance field in a struct body. 
+    // You can initialize struct members only by using a parameterized constructor 
+    // or by accessing the members individually after the struct is declared. 
+    // Any private or otherwise inaccessible members can be initialized only in a constructor.
+
+    /*
+    * There is no inheritance for structs as there is for classes. 
+    * A struct cannot inherit from another struct or class, and it cannot be the base of a class.
+    * Structs, however, inherit from the base class Object. 
+    * A struct can implement interfaces, and it does that exactly as classes do.
+    */
+  
+public struct GPSCoordinate
+{
+private readonly double latitude;
+private readonly double longitude;
+
+public double Latitude
+{
+    get { return latitude; }
+}
+
+public double Longitude
+{
+    get { return longitude; }
+}
+
+public GPSCoordinate(double latitude, double longitude)
+{
+    this.latitude = latitude;
+    this.longitude = longitude;
+}
+
+public override string ToString()
+{
+    return string.Format("{0},{1}", Latitude, Longitude);
+}
+}
+}
---
+++ ClassLibrary1/Model/LunchPlace.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eat.Core.Model
+{
+    public class LunchPlace
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Website
+        {
+            get;
+            set;
+        }
+
+        public GPSCoordinate Location
+        {
+            get;
+            set;
+        }
+    }
+}
